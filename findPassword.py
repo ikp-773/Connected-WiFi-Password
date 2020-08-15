8f5efbe388b33d98392238c980dd36a7a3f84fec
ikp-773 <abhijithkp773@gmail.com>
2020-08-16 04:36:43 +0530

    commit

--- findPassword.py
+++ findPassword.py
@@ -6,7 +6,6 @@ for i in profiles:
     results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i,
                         'key=clear']).decode('utf-8').split('\n')
     results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
-    # printing the profiles(wifi name) with their passwords using
     try:
         print ("{:<30}|  {:<}".format(i, results[0]))
     except IndexError:
