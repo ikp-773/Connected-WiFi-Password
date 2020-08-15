c44f3ee575490600ff1b1a12f80fd2976ca53210
ikp-773 <abhijithkp773@gmail.com>
2020-08-16 04:34:49 +0530

    Initial commit

---
+++ findPassword.py
@@ -0,0 +1,15 @@
+import subprocess
+data=subprocess.check_output(['netsh','wlan','show','profiles']).decode('utf-8',errors="backlashreplace").split('\n')
+profiles=[i.split(":")[1][1:-1]for i in data if "All User Profile" in i]
+
+for i in profiles:
+    results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i,
+                        'key=clear']).decode('utf-8').split('\n')
+    # storing passwords after converting them to list
+    results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+    # printing the profiles(wifi name) with their passwords using
+    # try and except method
+    try:
+        print ("{:<30}|  {:<}".format(i, results[0]))
+    except IndexError:
+        print ("{:<30}|  {:<}".format(i, ""))
